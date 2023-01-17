36f6c4a7b59f2aed8a61d0388a43399cbb47b932
NigarAdigozelova <72kwbg1@code.edu.az>
2023-01-17 12:40:10 +0400

    sffsdfdf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nigar Adigozelova
+          Love u
         </p>
         <a
           className="App-link"
